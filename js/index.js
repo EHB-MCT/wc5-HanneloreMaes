9e9b60ed161735882bc7d8c879687377eeaae26c
Hannelore Maes <hannelore.maes@student.ehb.be>
2021-03-18 12:03:45 +0100

    Start + save token oki + login oki

--- js/index.js
+++ js/index.js
@@ -16,6 +16,11 @@ const messageSystem = {
 const userSystem = {
   token: "",
   loggedIn: false,
+  checkToken(){
+      if (this.token !== null){
+        userSystem.updateUser();
+      }
+    },
 
   saveToken() {
     localStorage.setItem("token", this.token);
@@ -31,16 +36,43 @@ const userSystem = {
 
   login(email, password) {
     // https://thecrew.cc/api/user/login.php POST
+    console.log(email, password);
+    const data = {email: email, password: password};                                                  /**/
+
+    fetch('https:thecrew.cc/api/user/login.php', {method: 'POST', body: JSON.stringify(data)})        /*stringify zorgt voor leesbare code*/
+      .then(response => response.json())                                                              /*response vragen aan json*/
+      .then(data => {console.log("Succes",data); 
+      this.token = data.token;                                                                        /*data.token is de token die ge krijgt bij inloggen*/
+      this.saveToken(); });                                                                           /*token in lokale storage plaatsen*/
   },
 
   updateUser(password, handle) {
     // https://thecrew.cc/api/user/update.php?token=__TOKEN__ POST
+    const dataHandle = {password: password, handle: handle};
+
+    fetch(`https://thecrew.cc/api/user/update.php?token=${this.token}`, {method: 'POST'})
+    .then(response => response.json());
+    
   }
+
+  
 };
 
 const display = {
   initFields() {
+    const form = document.getElementById("loginForm");
+    form.addEventListener('submit', this.submitHandler);
+
+  },
+
+  submitHandler(e) {
+    e.preventDefault();
+    const email = document.getElementById("emailField").value;
+    const password = document.getElementById("passwordField").value;
+    
+    userSystem.login(email,password);
   }
 };
 
   display.initFields();
+  userSystem.checkToken();                                                          /*Bij inloggen token bijhouden en kijken of er een token aanwezig is en zo ja doorsturen naar pagina*/
